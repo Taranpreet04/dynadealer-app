6c53559625dd8e85735a5ab499527c64efd8fbf8
Taranpreet04 <taranpreet@shinedezign.com>
2025-04-07 11:52:19 +0530

    live design changes on customer portal make it responsive

--- extensions/theme-extension/assets/subscription.js
+++ extensions/theme-extension/assets/subscription.js
@@ -1,7 +1,7 @@
 console.log("js--________");
 
 let serverPath = "https://dynadealersapp.com";
-// let serverPath = "https://gsm-floating-fear-activated.trycloudflare.com";
+// let serverPath = "https://allow-projector-chip-environments.trycloudflare.com";
 let allProductId = [];
 let allOffers = [];
 let activeCurrency = Shopify?.currency?.active;
@@ -40,11 +40,9 @@ if (currentUrl.includes("account")) {
     let cusDiv = document.createElement("div");
     cusDiv.className = "mange-sub-container";
     cusDiv.innerHTML = `<div class='subscription-manage'>
-                <div>
                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50"  viewBox="0 0 24 24" fill="transparent">
                   <path d="M16 17H21M18.5 14.5V19.5M12 19H6.2C5.0799 19 4.51984 19 4.09202 18.782C3.71569 18.5903 3.40973 18.2843 3.21799 17.908C3 17.4802 3 16.9201 3 15.8V8.2C3 7.0799 3 6.51984 3.21799 6.09202C3.40973 5.71569 3.71569 5.40973 4.09202 5.21799C4.51984 5 5.0799 5 6.2 5H17.8C18.9201 5 19.4802 5 19.908 5.21799C20.2843 5.40973 20.5903 5.71569 20.782 6.09202C21 6.51984 21 7.0799 21 8.2V11M20.6067 8.26229L15.5499 11.6335C14.2669 12.4888 13.6254 12.9165 12.932 13.0827C12.3192 13.2295 11.6804 13.2295 11.0677 13.0827C10.3743 12.9165 9.73279 12.4888 8.44975 11.6335L3.14746 8.09863" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                   </svg>
-                </div>
                 <h3>Manage Memberships</h3>
                 </div>`;
 
